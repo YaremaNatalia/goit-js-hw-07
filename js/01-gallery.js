1cdbd2abd35852b088b5a71ded57701c2f7c6ecf
YaremaNatalia <yaremka.n@gmail.com>
2023-04-23 08:01:27 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,25 +28,30 @@ function createPhotosMarkup(images) {
 }
 
 imagesContainer.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  const isImageEl = evt.target.classList.contains("gallery__image");
+  evt.preventDefault(); // заборона автоматичного збереження фото на компьютер
+  const isImageEl = evt.target.classList.contains("gallery__image"); // перевірка, щоб клік був саме на фото а не на проміжки між ними
   if (!isImageEl) {
     return;
   }
-  const imgOriginal = evt.target.getAttribute("data-source");
+  const imgOriginal = evt.target.getAttribute("data-source"); //посилання на велике зображення
+  //створення модального вікна через метод бібліотеки з виведенням фото великого розміру
   const instance = basicLightbox.create(`
     <img src="${imgOriginal}" width="800" height="600">
-`);
-  instance.show();
+  `);
 
-    document.addEventListener("keydown", (evt) => onEscPress(evt, instance));
+  instance.show(); //відкриття модалки методом бібліотеки
+
+  const onEscPress = (evt) => {
+    const ESC_KEYCODE = "Escape";
+    if (evt.code === ESC_KEYCODE) {
+      instance.close();
+      document.removeEventListener("keydown", onEscPress); //функція яка закриває модалку клавішею "Escape" і знімає слухач клавіатури
+    }
+  };
+
+  document.addEventListener("keydown", onEscPress); //додавання слухача клавіатури і виклик функції перевірки чи натиснута клавіша "Escape". Стоїть нижче бо функція обявлена через константу
+
+  instance.on("close", () => {
+    document.removeEventListener("keydown", onEscPress); //додавання слухача "закриття" на модальне вікно методом бібліотеки, якщо модалка закривається викликається зняття слухача клавіатури (при знятті вказуються ті ж самі параметри, що і при додаванні)
+  });
 });
-
-const onEscPress = (evt, instance) => {
-  const ESC_KEYCODE = "Escape";
-  if (evt.code === ESC_KEYCODE) {
-    instance.close();
-
-    document.removeEventListener("keydown", (evt) => onEscPress(evt, instance));
-  }
-};
