bfd49ad599a4de2491599d06112c12aec37aa99a
YaremaNatalia <yaremka.n@gmail.com>
2023-04-24 13:57:35 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,30 +28,65 @@ function createPhotosMarkup(images) {
 }
 
 imagesContainer.addEventListener("click", (evt) => {
-  evt.preventDefault(); // заборона автоматичного збереження фото на компьютер
-  const isImageEl = evt.target.classList.contains("gallery__image"); // перевірка, щоб клік був саме на фото а не на проміжки між ними
+  evt.preventDefault();
+  const isImageEl = evt.target.classList.contains("gallery__image");
   if (!isImageEl) {
     return;
   }
-  const imgOriginal = evt.target.getAttribute("data-source"); //посилання на велике зображення
-  //створення модального вікна через метод бібліотеки з виведенням фото великого розміру
-  const instance = basicLightbox.create(`
+  const imgOriginal = evt.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(
+    `
     <img src="${imgOriginal}" width="800" height="600">
-  `);
+  `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEscPress); // додаємо слухач клавіатури використовуючи власнивість обєкту бібліотеки
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEscPress); // знімаємо слухач клавіатури використовуючи власнивість обєкту бібліотеки
+      },
+    }
+  );
+  instance.show();
 
-  instance.show(); //відкриття модалки методом бібліотеки
-
-  const onEscPress = (evt) => {
+  function onEscPress(evt) {
     const ESC_KEYCODE = "Escape";
     if (evt.code === ESC_KEYCODE) {
-      instance.close();
-      document.removeEventListener("keydown", onEscPress); //функція яка закриває модалку клавішею "Escape" і знімає слухач клавіатури
+      instance.close(); // закриття модального вікна
     }
-  };
-
-  document.addEventListener("keydown", onEscPress); //додавання слухача клавіатури і виклик функції перевірки чи натиснута клавіша "Escape". Стоїть нижче бо функція обявлена через константу
-
-  instance.on("close", () => {
-    document.removeEventListener("keydown", onEscPress); //додавання слухача "закриття" на модальне вікно методом бібліотеки, якщо модалка закривається викликається зняття слухача клавіатури (при знятті вказуються ті ж самі параметри, що і при додаванні)
-  });
+  }
 });
+
+// imagesContainer.addEventListener("click", (evt) => {
+//   evt.preventDefault(); // заборона автоматичного збереження фото на компьютер
+//   const isImageEl = evt.target.classList.contains("gallery__image"); // перевірка, щоб клік був саме на фото а не на проміжки між ними
+//   if (!isImageEl) {
+//     return;
+//   }
+//   const imgOriginal = evt.target.getAttribute("data-source"); //посилання на велике зображення
+//   //створення модального вікна через метод бібліотеки з виведенням фото великого розміру
+//   const instance = basicLightbox.create(
+//     `
+//     <img src="${imgOriginal}" width="800" height="600">
+//   `,
+//     {
+//       onShow: (instance) => {
+//         document.addEventListener("keydown", onEscPress);
+//       },
+//     }
+//   );
+
+//   instance.show(); //відкриття модалки методом бібліотеки
+
+//   const onEscPress = (evt) => {
+//     const ESC_KEYCODE = "Escape";
+//     if (evt.code === ESC_KEYCODE) {
+//       instance.close();
+//       document.removeEventListener("keydown", onEscPress); //функція яка закриває модалку клавішею "Escape" і знімає слухач клавіатури
+//     }
+//   };
+
+//   document.addEventListener("keydown", onEscPress); //додавання слухача клавіатури і виклик функції перевірки чи натиснута клавіша "Escape". Стоїть нижче бо функція обявлена через константу
+
+// });
